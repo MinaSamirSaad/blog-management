bd8795f2802b097f4f8a333939fb20f189676882
MinaSamirSaad <minasamir.nashid@gmail.com>
2025-02-14 19:31:23 +0200

    fix swagger ui on vercel

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as express from 'express';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  // Serve static files for Swagger UI
+  app.use('/api/docs', express.static(join(__dirname, '..', 'node_modules/swagger-ui-dist')));
+  app.useStaticAssets(join(__dirname, '..', 'node_modules/swagger-ui-dist'));
   // Swagger Configuration
   const config = new DocumentBuilder()
     .setTitle('API documentation')
