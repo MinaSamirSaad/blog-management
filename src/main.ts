dc9cce5f4cc16e32826e070b8939736f5d7992ee
MinaSamirSaad <minasamir.nashid@gmail.com>
2025-02-14 21:35:00 +0200

    fix swagger ui on vercel

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,12 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document, {
-    customSiteTitle: 'API Docs',
+    customSiteTitle: 'API Docs', // Customize the Swagger UI title
+    customCss: '.swagger-ui .topbar { display: none }', // Customize the Swagger UI CSS
+    customJs: [
+      'https://unpkg.com/swagger-ui-dist@4.15.5/swagger-ui-bundle.js', // Load SwaggerUIBundle from CDN
+      'https://unpkg.com/swagger-ui-dist@4.15.5/swagger-ui-standalone-preset.js', // Load SwaggerUIStandalonePreset from CDN
+    ],
   });
 
   await app.listen(3000);
